4c826b740946e642a14e3acf9927a912281a4e40
Trigueiro <neto.paladinus@gmail.com>
2019-12-02 15:30:32 -0300

    feat: adiciona angular material e componente de teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
