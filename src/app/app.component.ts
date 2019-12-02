2b64a43b05c1390949ae0c79493a584f8233a493
Trigueiro <neto.paladinus@gmail.com>
2019-12-02 16:34:59 -0300

    feat: adiciona listagem de categorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'stf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   appTitle = 'Money App';
+
+  categoryList: Array<Category> = new Array();
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.getCategoryList();
+    }, 2000);
+  }
+
+  getCategoryList() {
+    this.categoryList = new Array(
+      {
+        id: 1,
+        nome: "Lazer"
+      },
+      {
+        id: 2,
+        nome: "Alimentação"
+      },
+      {
+        id: 3,
+        nome: "Supermercado"
+      },
+      {
+        id: 4,
+        nome: "Farmácia"
+      },
+      {
+        id: 5,
+        nome: "Estudo"
+      },
+      {
+        id: 6,
+        nome: "Outros"
+      }
+    );
+  }
+}
+
+export interface Category {
+  id: number,
+  nome: string
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
 
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
